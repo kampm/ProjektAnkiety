318e3c5c0dceeace2dd239cc9e04e12221ef2ba0
Almas <eleron888@gmail.com>
2020-04-20 05:44:56 +0200

    little changes

--- SurveyTool/Migrations/202004131622496_init.Designer.cs
+++ SurveyTool/Migrations/202004131622496_init.Designer.cs
@@ -7,13 +7,13 @@ namespace SurveyTool.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
-    public sealed partial class InitialCreate1 : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "202004041529382_InitialCreate1"; }
+            get { return "202004131622496_init"; }
         }
         
         string IMigrationMetadata.Source
