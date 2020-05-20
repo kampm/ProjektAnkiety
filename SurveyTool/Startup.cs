a498ee81bc58d9113470d2ef73f335bf83ba7db7
Almas <eleron888@gmail.com>
2020-05-20 07:48:38 +0200

    created swagger config for future testing and documentation API. But now I have some problems with setting swagger for .NETFramework

--- SurveyTool/Startup.cs
+++ SurveyTool/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Swashbuckle.Application;
+using System.Web.Http;
 
 [assembly: OwinStartupAttribute(typeof(SurveyTool.Startup))]
 namespace SurveyTool
@@ -8,6 +10,9 @@ namespace SurveyTool
     {
         public void Configuration(IAppBuilder app)
         {
+            var config = new HttpConfiguration();
+            config.EnableSwagger(c => c.SingleApiVersion("v1", "A title for your API")).EnableSwaggerUi();
+            //app.Use(config);
             ConfigureAuth(app);
         }
     }
